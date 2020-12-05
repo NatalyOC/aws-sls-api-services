44a642e69e752d8a9d2197c57056c31923c1cc9e
NatalyOteroCelis <notero@pandero.com.pe>
2020-12-05 18:37:19 -0500

    configurando punto de entrada que invoca lambda

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 module.exports.hello = (event,context,callback) => {
  const response={
@@ -12,4 +12,13 @@ module.exports.hello = (event,context,callback) => {
     return callback(null,response);
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+};*/
+
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./src/index');
+
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+}
